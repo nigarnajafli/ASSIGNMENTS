520e9ef70c9206f5330592b1a62b6cc91bc034dc
unknown <nigar.najafli.std@bhos.edu.az>
2020-09-25 13:13:48 +0400

    Week 2 assignments

---
+++ WEEK2/assignment.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void swap (int val1, int val2);
+
+void print (int *val1, int *val2);
+
+int main(){
+
+	int i = 10;
+	int j = 20;
+
+	print(&i, &j);
+
+	swap(i,j);
+
+	printf("Exiting swap function:\n");
+
+	print(&i, &j);
+	
+	return 0;
+}
+
+void swap (int val1, int val2){
+
+	int temp;
+	printf("Inside swap function:\n");
+	print(&val1, &val2);
+	temp = val1;
+	val1 = val2;
+	val2 = temp;
+	printf("After swap function:\n");
+	print(&val1, &val2);
+}
+
+void print (int *val1, int *val2){
+
+	printf("i: %d is at %p\n", *val1, val1);
+	printf("j: %d is at %p\n", *val2, val2);
+}
+
